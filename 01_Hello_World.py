926bb3f823dce57fecde984c222fbcfb8445076a
Shubh0794 <36176287+Shubh0794@users.noreply.github.com>
2021-06-10 11:16:50 -0400

    Pull in these changes
    New changes on the master branch

--- 01_Hello_World.py
+++ 01_Hello_World.py
@@ -3,3 +3,6 @@ print ("This is the first time that I have used vim in mac terminal")
 print ("Gotta say it's fun. Bye!")
 
 
+def another_dummy_function():
+	print("Another dummy function text goes here!)
+	
